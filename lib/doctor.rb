7d9c4eb31d058f7994018f12a2d4885c11a2b1e0
Mohammed Chisti <Chisti@Mohammeds-MacBook-Air.local>
2016-12-04 20:11:33 -0500

    Done.

---
+++ lib/doctor.rb
@@ -0,0 +1,14 @@
+class Doctor
+  attr_accessor :appointments, :patients
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @appointments = []
+    @patients = []
+  end
+
+  def add_appointment(appointment)
+    appointments << appointment
+  end
+end
